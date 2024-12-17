05031f4a2eacc9da4b9eba2e1a225d0a2b502dc1
Sukhdeb Patra <sukhdebpatra1@gmail.com>
2024-12-17 13:10:42 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import "./App.css";
+import FormValidation from "./Pages/FormValidation";
+
+
+
+function App() {
+  
+
+  return (
+    <>
+   <FormValidation/>
+    </>
+  );
+}
+
+export default App;
